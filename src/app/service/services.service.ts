d0c7a53d2dabcdad2f57012ea01e8c69553a82dd
udaya <ping.udaya@gmail.com>
2023-06-26 00:05:56 +0545

    fix: seller registration

--- src/app/service/services.service.ts
+++ src/app/service/services.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { SellerSignUp } from '../datatype';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, tap } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Injectable({
@@ -11,36 +11,25 @@ export class ServicesService {
 
   isSellerSignedIn = new BehaviorSubject<boolean>(false)
 
-  constructor(private http: HttpClient, private router:Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
-  // userSignUp(data: SellerSignUp) {
-  //   try{
-  //   this.http
-  //     .post('http://localhost:3000/seller', data, { observe: 'response' })
-  //     .subscribe((result) => {
-  //       console.log("result", result)
-  //       this.router.navigate(['seller-home'])
-  //       this.isSellerSignedIn.next(true);
-  //     })
-  //   } catch(e){
-  //     console.log(e);
-      
-  //   }
-
-  //   return false;
-
-  // }
-  async userSignUp(data: SellerSignUp) {
+  userSignUp(data: SellerSignUp) {
     try {
-      const result = await this.http.post('http://localhost:3000/seller', data, { observe: 'response' }).toPromise();
-      console.log("result", result);
-      this.router.navigate(['seller-home']);
+      this.http
+        .post('http://localhost:3000/seller', data, { observe: 'response' })
+
+
+      this.router.navigate(['seller-home'])
       this.isSellerSignedIn.next(true);
-    } catch (error) {
-      // Handle error
-      console.error(error);
+    } catch (e) {
+      console.log(e);
+
     }
+
     return false;
+
+
+
   }
-  
+
 }
